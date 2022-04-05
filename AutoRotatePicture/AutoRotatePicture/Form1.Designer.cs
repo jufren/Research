fd854191ca9676ce16b00f8ad827d6a5854e5913
jufren <jufren@gmail.com>
2022-04-05 11:40:43 +0800

    add autorotate picture project
    add autorotate picture project

---
+++ AutoRotatePicture/AutoRotatePicture/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnAutoRotate = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(679, 0);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(916, 409);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(673, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "C:\\Users\\16898448_admin\\Downloads\\20220324_134900.jpg";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(760, -1);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnAutoRotate
+            // 
+            this.btnAutoRotate.Location = new System.Drawing.Point(841, -1);
+            this.btnAutoRotate.Name = "btnAutoRotate";
+            this.btnAutoRotate.Size = new System.Drawing.Size(75, 23);
+            this.btnAutoRotate.TabIndex = 4;
+            this.btnAutoRotate.Text = "Auto-rotate";
+            this.btnAutoRotate.UseVisualStyleBackColor = true;
+            this.btnAutoRotate.Click += new System.EventHandler(this.btnAutoRotate_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Image Files(*.bmp;*.jpg;*.gif;*.jpeg)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 450);
+            this.Controls.Add(this.btnAutoRotate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnAutoRotate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ AutoRotatePicture/AutoRotatePicture/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bmp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult r=openFileDialog1.ShowDialog();
+            if (r == DialogResult.OK)
+            {
+                textBox1.Text=openFileDialog1.FileName;
+                pictureBox1.Image = new System.Drawing.Bitmap(textBox1.Text);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FileInfo fi = new FileInfo(@textBox1.Text);
+           
+            string oriFileName = Path.GetFileNameWithoutExtension(textBox1.Text);
+            string newFileName = oriFileName + "a" + fi.Extension;
+            string newFilePath = @textBox1.Text.Replace(oriFileName + fi.Extension, newFileName);
+            bmp.Save(newFilePath);
+            MessageBox.Show("New Image saved " + newFilePath);
+        }
+        
+        private void btnAutoRotate_Click(object sender, EventArgs e)
+        {
+
+            
+            bmp= new System.Drawing.Bitmap(textBox1.Text);
+            pictureBox1.Image = FixImageOrientation(@textBox1.Text, bmp);            
+            
+        }
+        public Bitmap FixImageOrientation(string filename, Bitmap srce)
+        {
+            const int ExifOrientationId = 0x112;
+            // Read orientation tag
+            if (!srce.PropertyIdList.Contains(ExifOrientationId)) return srce;
+            var prop = srce.GetPropertyItem(ExifOrientationId);
+            var orient = BitConverter.ToInt16(prop.Value, 0);
+            // Force value to 1
+            prop.Value = BitConverter.GetBytes((short)1);
+            srce.SetPropertyItem(prop);
+            
+            // Rotate/flip image according to <orient>
+            switch (orient)
+            {
+                case 1:
+                    srce.RotateFlip(RotateFlipType.RotateNoneFlipNone);
+                    return srce;
+
+
+                case 2:
+                    srce.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    return srce;
+
+                case 3:
+                    srce.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                    return srce;
+
+                case 4:
+                    srce.RotateFlip(RotateFlipType.Rotate180FlipX);
+                    return srce;
+
+                case 5:
+                    srce.RotateFlip(RotateFlipType.Rotate90FlipX);
+                    return srce;
+
+                case 6:
+                    srce.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                    return srce;
+
+                case 7:
+                    srce.RotateFlip(RotateFlipType.Rotate270FlipX);
+                    return srce;
+
+                case 8:
+                    srce.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                    return srce;
+
+                default:
+                    srce.RotateFlip(RotateFlipType.RotateNoneFlipNone);
+                    return srce;
+            }
+        }
+    }
+}
